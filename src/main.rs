89b84feaf814f754fdd89fb056e35e5099acfe27
Koliras <gyrmanchyk230506@gmail.com>
2024-07-13 11:20:41 +0000

    create api url

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use std::fs;
 const ENGLISH_WIKI_URL_BASE: &str = "https://residentevil.fandom.com/wiki/";
 const UKRAINIAN_WIKI_URL_BASE: &str = "https://resident-evil.fandom.com/uk/wiki/";
 
+const ENGLISH_API_BASE_URL: &str = "https://residentevil.fandom.com/api.php?action=query&prop=revisions&rvprop=content&format=json&rvslots=main";
+const UKRAINIAN_API_BASE_URL: &str = "https://resident-evil.fandom.com/uk/api.php?action=query&prop=revisions&rvprop=content&format=json&rvslots=main";
+
 fn main() {
     let mut source_link = String::new();
     let mut dest_link = String::new();
@@ -21,14 +24,11 @@ fn main() {
         let source_title = extract_article_title(&source_link, ENGLISH_WIKI_URL_BASE);
         let dest_title = extract_article_title(&dest_link, UKRAINIAN_WIKI_URL_BASE);
 
-        println!("first link: ");
-        println!("{}", source_link);
-        println!("second link: ");
-        println!("{}", dest_link);
-        println!("source title: ");
-        println!("{}", source_title);
-        println!("dest title: ");
-        println!("{}", dest_title);
+        let eng_api_url = format!("{}&titles={}%2Fgallery", ENGLISH_API_BASE_URL, source_title);
+        let ukr_api_url = format!("{}&titles={}%2Fгалерея", UKRAINIAN_API_BASE_URL, dest_title);
+
+        println!("eng api url: {}", eng_api_url);
+        println!("ukr api url: {}", ukr_api_url);
 
         if dest_link != String::new() {
             source_link = String::new();
