6cf5cb3925bcbdf413facd42f0c91ecc18b3e652
Koliras <gyrmanchyk230506@gmail.com>
2024-07-13 10:49:22 +0000

    extract article name

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::fs;
 
+const ENGLISH_WIKI_URL_BASE: &str = "https://residentevil.fandom.com/wiki/";
+const UKRAINIAN_WIKI_URL_BASE: &str = "https://resident-evil.fandom.com/uk/wiki/";
+
 fn main() {
     let mut source_link = String::new();
     let mut dest_link = String::new();
@@ -15,13 +18,31 @@ fn main() {
             continue;
         }
 
+        let source_title = extract_article_title(&source_link, ENGLISH_WIKI_URL_BASE);
+        let dest_title = extract_article_title(&dest_link, UKRAINIAN_WIKI_URL_BASE);
+
         println!("first link: ");
         println!("{}", source_link);
         println!("second link: ");
         println!("{}", dest_link);
+        println!("source title: ");
+        println!("{}", source_title);
+        println!("dest title: ");
+        println!("{}", dest_title);
+
         if dest_link != String::new() {
             source_link = String::new();
             dest_link = String::new();
         }
     }
+}
+
+fn extract_article_title(link: &String, base_url: &str) -> String {
+    let mut url = link[base_url.len()..].to_string();
+    url = url[0..url
+        .chars()
+        .position(|c| c == '/' || c == '?')
+        .unwrap_or_else(|| return url.len())]
+        .to_string();
+    return url;
 }
