a91ce1e5d7d323c190d79198abe3dad1006f3990
Koliras <gyrmanchyk230506@gmail.com>
2024-07-13 10:03:56 +0000

    change link vars names

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
 use std::fs;
 
 fn main() {
-    let mut first_link = String::new();
-    let mut second_link = String::new();
+    let mut source_link = String::new();
+    let mut dest_link = String::new();
     for line in fs::read_to_string("./input.txt").unwrap().lines() {
-        if first_link != String::new() {
-            second_link = line.to_string();
+        if source_link != String::new() {
+            dest_link = line.to_string();
         }
-        if first_link == String::new() {
-            first_link = line.to_string();
+        if source_link == String::new() {
+            source_link = line.to_string();
         }
 
-        if second_link == String::new() {
+        if dest_link == String::new() {
             continue;
         }
 
         println!("first link: ");
-        println!("{}", first_link);
+        println!("{}", source_link);
         println!("second link: ");
-        println!("{}", second_link);
-        if second_link != String::new() {
-            first_link = String::new();
-            second_link = String::new();
+        println!("{}", dest_link);
+        if dest_link != String::new() {
+            source_link = String::new();
+            dest_link = String::new();
         }
     }
 }
