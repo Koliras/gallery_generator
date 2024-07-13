9985fb215b4c8a1692019c468265e6b57f14a0ec
Koliras <gyrmanchyk230506@gmail.com>
2024-07-13 10:03:17 +0000

    add reading file with input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let mut first_link = String::new();
+    let mut second_link = String::new();
+    for line in fs::read_to_string("./input.txt").unwrap().lines() {
+        if first_link != String::new() {
+            second_link = line.to_string();
+        }
+        if first_link == String::new() {
+            first_link = line.to_string();
+        }
+
+        if second_link == String::new() {
+            continue;
+        }
+
+        println!("first link: ");
+        println!("{}", first_link);
+        println!("second link: ");
+        println!("{}", second_link);
+        if second_link != String::new() {
+            first_link = String::new();
+            second_link = String::new();
+        }
+    }
 }
